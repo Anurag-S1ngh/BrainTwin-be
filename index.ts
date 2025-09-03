d1fabd514614a4bdd68555d298840558aa100236
Anurag Singh <anuragsingh2313@gmail.com>
2025-09-04 01:53:58 +0530

    add rate limiter

--- index.ts
+++ index.ts
@@ -4,18 +4,30 @@ import mongoose from "mongoose";
 import { aiRouter } from "./routes/ai";
 import { authRouter } from "./routes/auth";
 import { contentRouter } from "./routes/content";
+import { rateLimit } from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (_req, res) => {
+    res.status(429).json({ message: "Too many requests" });
+  },
+});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/content", contentRouter);
 app.use("/api/v1/ai", aiRouter);
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1/my_database");
+  await mongoose.connect("mongodb://localhost:27017");
   app.listen(3000);
 }
 await main();
